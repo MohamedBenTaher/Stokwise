8a2b30165bac318c270ad5c4b0ebd9ade9aebbb4
Mohamed Ben Taher <mbentaher1998@gmail.com>
2024-03-31 15:40:24 +0000

    Add new files and configurations for frontend and backend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+import defaultConfig from 'tailwindcss/defaultConfig'
+import formsPlugin from '@tailwindcss/forms'
+/** @type {import('tailwindcss/types').Config} */
+export default {
+	content: ['index.html', 'src/**/*.tsx'],
+	theme: {
+		fontFamily: {
+			sans: ['Inter', ...defaultConfig.theme.fontFamily.sans]
+		}
+	},
+	experimental: { optimizeUniversalDefaults: true },
+	plugins: [formsPlugin]
+}
