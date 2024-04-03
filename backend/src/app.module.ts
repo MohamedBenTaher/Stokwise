9760e92110fac80be2c68119562b66bc806e3cea
Mohamed Ben Taher <mbentaher1998@gmail.com>
2024-04-03 23:21:35 +0000

    Add new files and modules for authentication, roles, mail, and i18n

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from 'src/database/typorm-config.service';
@@ -9,7 +9,12 @@ import { TypeOrmConfigService } from 'src/database/typorm-config.service';
 import databseConfig from 'src/database/config/databse.config';
 
 import { config } from 'dotenv';
-import { resolve } from 'path';
+import path, { resolve } from 'path';
+import { MailerModule } from './mailer/mailer.module';
+import { MailModule } from './mail/mail.module';
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import { AllConfigType } from './config/config.type';
+import { AuthModule } from './auth/auth.module';
 
 config({ path: resolve(__dirname, '.env') });
 
@@ -23,6 +28,32 @@ config({ path: resolve(__dirname, '.env') });
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService<AllConfigType>) => ({
+        fallbackLanguage: configService.getOrThrow('app.fallbackLanguage', {
+          infer: true,
+        }),
+        loaderOptions: { path: path.join(__dirname, '/i18n/'), watch: true },
+      }),
+      resolvers: [
+        {
+          use: HeaderResolver,
+          useFactory: (configService: ConfigService<AllConfigType>) => {
+            return [
+              configService.get('app.headerLanguage', {
+                infer: true,
+              }),
+            ];
+          },
+          inject: [ConfigService],
+        },
+      ],
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+    MailerModule,
+    MailModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/config/config.type.ts
+++ backend/src/config/config.type.ts
@@ -1,5 +1,9 @@
 import { DatabaseConfig } from 'src/database/config/database-config.type';
+import { AppConfig } from './app-config.type';
+import { MailConfig } from 'src/mail/config/mail-config.type';
 
 export type AllConfigType = {
   database: DatabaseConfig;
+  app: AppConfig;
+  mail: MailConfig;
 };
